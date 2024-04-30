dee629bb75b2bad08164e4ad3bb15dc74f520967
Damian Slomian <d.slomian@bokonet.de>
2024-04-30 09:25:15 +0200

    version 12.0.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,5 +29,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'bookingtime',
     'author_email' => 'cms-ext@bookingtime.com',
     'author_company' => 'bookingtime GmbH',
-    'version' => '12.0.2',
+    'version' => '12.0.3',
 ];
